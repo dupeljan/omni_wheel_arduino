e8149e99d61944904c38ab4058e2887e6b35b864
Dupeljan Linux <dupeljan@gmail.com>
2019-11-21 09:19:25 +0300

    Add rotation

--- origin.ino
+++ origin.ino
@@ -13,22 +13,36 @@
 
 #define EPS 1e-3
 #define PI 3.14159265359
-#define WHEEL_COUNT 4
+#define DELTA 0.78539816339
 
 
 class Movements{
-int wheel_count = WHEEL_COUNT;
-double rot_angle; // rad
-double alpha[WHEEL_COUNT];  // rad
-double delta[WHEEL_COUNT];  // rad
-double r_rot_point[WHEEL_COUNT];  // cm
-double wheel_r;                   // cm 
+static const int wheel_count = 4;
+const int precision = 1000;
+double rot_angle = 0.; // rad
+double alpha[wheel_count] = { PI/3, 5*PI/6, 8*PI/6, 11*PI/6 };  // rad
+double delta[wheel_count] = { DELTA, -DELTA, -DELTA, DELTA };  // rad
+double r_rot_point[wheel_count] = { 5., 5., 5., 5. };  // cm
+double wheel_r = 0.5;                   // cm 
+double angular_v[wheel_count];    // rad/sec
+double v_angl_previous = 0.;            // rad/sec
+double vx_previous;               // cm/sec
+double vy_previous;               // cm/sec
 long t_previous_call;       // sec * 10^6
+long t_ang_v_comp = 0;      // sec * 10^6
+
+void add_rot_angl(double angl);
 public: 
   Movements(){
-    rot_angle = 0.;
     t_previous_call = micros();
   }
+  
+  boolean is_rotating(){
+    return abs(v_angl_previous) > EPS;
+  }
+    
+  void move(double vx,double vy,double v_angl);
+  void move();  
 };
 
 
@@ -42,6 +56,7 @@ void setup() {
   pinMode(rIN3, OUTPUT);
   pinMode(rIN4, OUTPUT);
   Serial.begin(9600);
+  
 }
 
 
@@ -86,21 +101,30 @@ void setwheelspeed(int wheel, int speed)
   }
 }
 
+void Movements::add_rot_angl(double angl){
+  rot_angle += angl;
+  while (  abs_d(rot_angle) > 2 * PI)
+     rot_angle -= sgn(rot_angle)*2*PI; 
+}
 
-void move(double vx, double vy, double r){
-  const int wheel_count = 4;
-  const double R = 1; // wheel radius
-  const double rot_angle = 0;
-  const double delta = 0.78539816339; // pi/4
-  double angular_v[wheel_count];
-  const int precision = 1000;
+void Movements::move(){
+ move(vx_previous,vy_previous,v_angl_previous); 
+}
+
+void Movements::move(double vx,double vy,double v_angl){
+  vx_previous = vx;
+  vy_previous = vy; 
+  add_rot_angl(v_angl_previous * (t_ang_v_comp + micros()-t_previous_call) * 1e-6); // plus ang_v computing time
   
-  angular_v[1] = (vx * cos(rot_angle - delta) + vy*sin(rot_angle - delta) ) / (double)(R * cos(delta)); 
-  angular_v[0] = (vx * cos(rot_angle + delta) + vy*sin(rot_angle + delta) ) / (double)(R * cos(delta)); 
-  angular_v[3] = (vx * cos(rot_angle + delta) + vy*sin(rot_angle + delta) ) / (double)(R * cos(delta)); 
-  angular_v[2] = (vx * cos(rot_angle - delta) + vy*sin(rot_angle - delta) ) / (double)(R * cos(delta)); 
+  for(int i = 0; i < wheel_count; i++)
+     angular_v[i] = (vx * cos(rot_angle + delta[i]) + vy*sin(rot_angle + delta[i]) + r_rot_point[i]*v_angl*sin(alpha[i] - delta[i])) / (double)(wheel_r * cos(delta[i])); 
+  //angular_v[1] = (vx * cos(rot_angle - delta) + vy*sin(rot_angle - delta) ) / (double)(wheel_r * cos(delta)); 
+  //angular_v[0] = (vx * cos(rot_angle + delta) + vy*sin(rot_angle + delta) ) / (double)(wheel_r * cos(delta)); 
+  //angular_v[3] = (vx * cos(rot_angle + delta) + vy*sin(rot_angle + delta) ) / (double)(wheel_r * cos(delta)); 
+  //angular_v[2] = (vx * cos(rot_angle - delta) + vy*sin(rot_angle - delta) ) / (double)(wheel_r * cos(delta)); 
     
-    
+  v_angl_previous = v_angl;
+  t_previous_call = micros();  
     Serial.println(vx);
     Serial.println(vy);
   double v_max = max_abs(angular_v,wheel_count);
@@ -108,7 +132,8 @@ void move(double vx, double vy, double r){
     Serial.println( String(i));
     Serial.println(angular_v[i],4);
     Serial.println();
-    int v = abs(vx) + abs(vy);
+    
+    //int v = abs(vx) + abs(vy) + abs(r_rot_point_max*v_angl);
     setwheelspeed(i,sgn(angular_v[i])*map(abs((int)(angular_v[i]*precision)),0,(int)(precision * ( (v_max > EPS)? v_max: 1)),0,250));
   }
   
@@ -133,20 +158,27 @@ void rotate(bool dir)
 }
 
 void parse(String command, String x[]){
-  int i;
-  x[0]=x[1]="";
-  for(i = 2;command[i] != ' ';i++)x[0] += command[i];
-  for(;i < command.length();i++)x[1] += command[i];
+  const int count = 3;
+  int i,j;
+  for (i=0; i < count; i++)
+    x[i] = "";
+  for(j = 0,i = 2; j < count - 1; j++)
+    for(;command[i] != ' ';i++)x[j] += command[i];
+  for(;i < command.length();i++)x[count - 1] += command[i];
 }
 
 Vector vec;
 String command, x[2];
+Movements car;
 int i;
 void loop() {
   if (Serial.available())
   {
     command = Serial.readStringUntil('\n');
-    if(command[0] == 'L')
+    if (car.is_rotating()){
+     car.move(); 
+    }
+    else if(command[0] == 'L')
     {
       parse(command,x);
       vec.set_coordinates(atof(x[0].c_str()),atof(x[1].c_str()));
@@ -165,16 +197,7 @@ void loop() {
      Serial.println(x[1]);
      Serial.println();
      
-     move(atof(x[0].c_str()),atof(x[1].c_str()),0);
+     car.move(atof(x[0].c_str()),atof(x[1].c_str()),atof(x[2].c_str()));
     }
-    else if (command[0] == 'T'){
-        setwheelspeed(0,255);
-        delay(1000);
-        setwheelspeed(0,0);
-        delay(1000);
-        setwheelspeed(0,-255);
-        delay(1000);
-        setwheelspeed(0,0);
-    }  
   }
 }
